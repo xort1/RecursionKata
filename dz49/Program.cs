169deb740f196794735833828265ce7919956f6a
Daniil Gubanov <yranidrop@gmail.com>
2022-10-01 00:07:12 +0300

    Добавьте файлы проекта.

---
+++ dz49/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Project
+{
+    class Project
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; int i = 0, result = 0; string a = "561";
+
+            Print(array, i);
+            Sum(array, i, result);
+            SumOfNumbers(a, i, result);
+        }
+
+        static void Print(int[] array, int i)
+        {
+            if(i >= array.Length)
+                return;
+
+            Console.Write(array[i] + " ");
+            i++;
+            Print(array, i);
+        }
+
+        static void Sum(int[] array, int i, int result)
+        {
+            if (i >= array.Length)
+            {
+                Console.WriteLine("\nSum = " + result);
+                return;
+            }
+
+            result += array[i];
+            i++;
+            Sum(array, i, result);
+        }
+
+        static void SumOfNumbers(string a, int i, int result)
+        {
+            if (i >= a.Length)
+            {
+                Console.WriteLine("SumOfNumbers = " + result);
+                return;
+            }
+
+            result += a[i] - '0';
+            i++;
+            SumOfNumbers(a, i, result);
+        }
+    }
+}
